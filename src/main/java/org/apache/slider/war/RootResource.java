d160340f00c37efeb90c20b7fd2ab13f3cbaf6d9
venkateshp2026 <venkateshbabup2026@gmail.com>
2026-08-28 21:42:08 +0530

    bug2
    Comment out the import statements for GET and Path.

--- src/main/java/org/apache/slider/war/RootResource.java
+++ src/main/java/org/apache/slider/war/RootResource.java
@@ -1,7 +1,7 @@
 package org.apache.slider.war;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+//import javax.ws.rs.GET;
+//import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
