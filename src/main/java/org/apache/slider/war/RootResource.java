94bb613a79ff96415013b7c21c583f4131801023
venkateshp2026 <venkateshbabup2026@gmail.com>
2026-08-28 21:36:40 +0530

    bug introducing
    Commented out import statements for Path and Produces.

--- src/main/java/org/apache/slider/war/RootResource.java
+++ src/main/java/org/apache/slider/war/RootResource.java
@@ -1,8 +1,8 @@
 package org.apache.slider.war;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+//import javax.ws.rs.Path;
+//import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 /**
